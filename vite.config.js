1b253cc6dbb07f928b92393572044d6acbe6e031
nazhG <nazhirgonzalez@gmail.com>
2025-03-24 13:45:51 -0500

    .env

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+import process from 'process';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +15,5 @@ export default defineConfig({
     },
   },
   plugins: [tailwindcss(),react()],
+  base: process.env.VITE_BASE_URL || '/',
 })
